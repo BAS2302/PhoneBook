9d3b654cd0b33916667740a0bcfec5e5849e1d93
BelAnton <belanton@mail.ru>
2020-09-21 21:51:33 +0500

    add checkName method

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class PhoneBook {
 
     public static void main(String[] args) {
         //Добавить считывание ввода пользователя в цикле
+        System.out.println("Введите имя в формате ФИО");
+        Scanner scanner = new Scanner(System.in);
+        checkName(scanner.nextLine());
     }
 
     public static boolean checkPhoneNumber(String phoneNumber) {
@@ -9,7 +14,32 @@ public class PhoneBook {
     }
 
     public static boolean checkName(String name) {
-        return true;
+        String[] arrayName = name.trim().split(" ");
+        if (arrayName.length == 3) {
+            int famLen = arrayName[0].length();
+            if (arrayName[0].charAt(famLen-1) == 'а') {
+                arrayName[0] = arrayName[0].substring(0, famLen-1);
+            }
+            String famLastTwo = arrayName[0].substring(famLen - 2, famLen);
+            switch (famLastTwo) {
+                case "ов":
+                case "ев":
+                case "ин":
+                case "ын":
+                case "их":
+                case "ыч":
+                    return true;
+            }
+            String famLastFore = arrayName[0].substring(famLen - 4, famLen);
+            switch (famLastFore) {
+                case "ский":
+                case "ская":
+                case "цкий":
+                case "цкая":
+                    return true;
+            }
+        }
+        return false;
     }
 
     public static String formatName(String name) {
