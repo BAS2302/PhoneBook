53eb93c3ff8d392016aaab89be118009c46bf149
BelAnton <belanton@mail.ru>
2020-10-03 20:56:26 +0500

    fix mistakes

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -2,93 +2,134 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class PhoneBook {
-    static int stringNum = 0;
+//    static int stringNum = 1;
 
     public static void main(String[] args) {
         //Добавить считывание ввода пользователя в цикле
         Scanner scanner = new Scanner(System.in);
 
-        int columnNum = 2;
-        String[][] book = new String[stringNum][columnNum];
+//        int columnNum = 2;
+        String[][] book = new String[0][2];
         String name = "";
         String number = "";
         boolean isCheckName = false;
         boolean isCheckNumber = false;
         String answerEnd = "";
-        String answer = "";
-        do {
-            System.out.println("Желаете ввести имя или номер телефона? " +
-                    "Введите да если хотите ввести Имя и нет, если номер");
-            answer = scanner.nextLine();
 
-            if (answer.toLowerCase() == "да") {
-                System.out.println("Введите имя:");
+        do {
+            name = "";
+            number = "";
+            System.out.println("Введите имя или номер телефона!");
+            String enter = scanner.nextLine();
+            String checkEnter = enter.trim().replaceAll("[^0-9]", "");
+
+            if (checkEnter.length() < 1) {
+                name = enter;
+                isCheckName = checkName(name);
                 while (!isCheckName) {
+                    if (!isCheckName) System.out.println("Введите корректное имя!");
                     name = scanner.nextLine();
                     isCheckName = checkName(name);
-                    if (!isCheckName) System.out.println("Введите корректное имя!");
                 }
-                for (int i = 0; i < stringNum; i++) {
-                    if (book[i][0] == name) {
-                        System.out.println("Это имя есть в справочнике!" + name + ": " + book[i][1]);
-                    } else if (i == stringNum - 1) {
-                        System.out.println("Такого имени нет в справочнике! Введите номер телефона");
-                        while (!isCheckNumber) {
-                            number = scanner.nextLine();
-                            isCheckNumber = checkPhoneNumber(number);
-                            if (!isCheckNumber) System.out.println("Введите корректный номер телефона!");
-                        }
-                        add(book, name, number);
-/*
-                        stringNum += 1;
-                        book = Arrays.copyOf(book, stringNum);
-                        book[stringNum - 1][0] = name;
-                        book[stringNum - 1][1] = number;
-*/
-                    }
-                }
-
+                isCheckName = false;
+                name = formatName(name);
             } else {
-                System.out.println("Введите номер:");
+                number = enter;
+                isCheckNumber = checkPhoneNumber(number);
                 while (!isCheckNumber) {
+                    if (!isCheckNumber) System.out.println("Введите корректный номер телефона!");
                     number = scanner.nextLine();
                     isCheckNumber = checkPhoneNumber(number);
-                    if (!isCheckNumber) System.out.println("Введите корректный номер телефона!");
                 }
-                for (int i = 0; i < stringNum; i++) {
-                    if (book[i][1] == number) {
-                        System.out.println("Этот номер есть в справочнике!" + book[i][0] + ": " + number);
-                    } else if (i == stringNum - 1) {
-                        System.out.println("Такого номера нет в справочнике! Введите имя");
-                        while (!isCheckName) {
-                            name = scanner.nextLine();
-                            isCheckName = checkName(name);
-                            if (!isCheckName) System.out.println("Введите корректное имя!");
+                isCheckNumber = false;
+                number = formatPhoneNumber(number);
+            }
+
+            System.out.println(book.length);
+
+            if (book.length == 0) {
+                if (name.length() > 1) {
+                    System.out.println("Такого имени нет в справочнике");
+                    System.out.println("Введите номер");
+                    while (!isCheckNumber) {
+                        number = scanner.nextLine();
+                        isCheckNumber = checkPhoneNumber(number);
+                        if (!isCheckNumber) System.out.println("Введите корректный номер телефона!");
+                    }
+                    isCheckNumber = false;
+                    number = formatPhoneNumber(number);
+                    book = new String[][]{{name,number}};
+                    System.out.println("Данные добавлены в справочник");
+                } else {
+                    System.out.println("Такого номера нет в справочнике");
+                    System.out.println("Введите имя");
+                    while (!isCheckName) {
+                        name = scanner.nextLine();
+                        isCheckName = checkName(name);
+                        if (!isCheckName) System.out.println("Введите корректное имя!");
+                    }
+                    isCheckName = false;
+                    name = formatName(name);
+                    book = new String[][]{{name,number}};
+                    System.out.println("Данные добавлены в справочник");
+                }
+            } else {
+                if (name.length() > 1) {
+                    for (int i = 0; i < book.length; i++) {
+                        if (book[i][0].equals(name)) {
+                            System.out.println("Такое имя есть в справочнике");
+                            System.out.println(book[i][0] + ": " + book[i][1]);
+                        } else {
+                            if (i == book.length - 1) {
+                                System.out.println("Такого имени нет в справочнике");
+                                System.out.println("Введите номер");
+                                while (!isCheckNumber) {
+                                    number = scanner.nextLine();
+                                    isCheckNumber = checkPhoneNumber(number);
+                                    if (!isCheckNumber) System.out.println("Введите корректный номер телефона!");
+                                }
+                                isCheckNumber = false;
+                                number = formatPhoneNumber(number);
+                                add(book, name, number);
+                            }
                         }
+                    }
 
-
-                        add(book, name, number);
-/*
-                        book = Arrays.copyOf(book, stringNum);
-                        book[stringNum - 1][0] = name;
-                        book[stringNum - 1][1] = number;
-*/
+                } else {
+                    for (int i = 0; i < book.length; i++) {
+                        if (book[i][1].equals(number)) {
+                            System.out.println("Такой номер есть в справочнике");
+                            System.out.println(book[i][0] + ": " + book[i][1]);
+                        } else {
+                            if (i == book.length - 1) {
+                                System.out.println("Такого номера нет в справочнике");
+                                System.out.println("Введите имя");
+                                while (!isCheckName) {
+                                    name = scanner.nextLine();
+                                    isCheckName = checkName(name);
+                                    if (!isCheckName) System.out.println("Введите корректное имя!");
+                                }
+                                isCheckName = false;
+                                name = formatName(name);
+                                add(book, name, number);
+                            }
+                        }
                     }
                 }
-
-
             }
 
             System.out.println("Хотите продолжить? Да/Нет");
             answerEnd = scanner.nextLine();
 
-        } while (answerEnd.toLowerCase() == "нет");
+        } while (answerEnd.toLowerCase().equals("да"));
+
+        list(book);
 
 
     }
 
     public static boolean checkPhoneNumber(String phoneNumber) {
-        String cleanTelNum = phoneNumber.replaceAll("[^0-9]", "");
+        String cleanTelNum = phoneNumber.trim().replaceAll("[^0-9]", "");
         int numLength = cleanTelNum.length();
         if (numLength == 11) {
             return true;
@@ -141,21 +182,39 @@ public class PhoneBook {
 
     public static String formatPhoneNumber(String number) {
         number = number.replaceAll("[^0-9]", "");
-        number = "+7" + number.substring(1, 4) + " " + number.substring(4, 7)
+        number = "+7" + " " + number.substring(1, 4) + " " + number.substring(4, 7)
                 + " " + number.substring(7, 9) + " " + number.substring(9, 11);
 
         return number;
     }
 
+
+
     public static void add(String[][] book, String name, String number) {
-        stringNum += 1;
-        book = Arrays.copyOf(book, stringNum);
-        book[stringNum - 1][0] = name;
-        book[stringNum - 1][1] = number;
-
-
+        book = addLength(book);
+        book[book.length - 1][0] = name;
+        book[book.length - 1][1] = number;
+        System.out.println(book[book.length - 1][0]);
         //add logic
     }
+
+    public static String[][] addLength(String[][] book) {
+        int arrayLen = book.length;
+        System.out.println(book.length);
+        String[][] copBook = new String[arrayLen][2];
+        for (int i = 0; i < arrayLen; i++) {
+            copBook[i][0] = book[i][0];
+            copBook[i][1] = book[i][1];
+        }
+        arrayLen += 1;
+        book = new String[arrayLen][2];
+        for (int i = 0; i < arrayLen - 1; i++) {
+            book[i][0] = copBook[i][0];
+            book[i][1] = copBook[i][1];
+        }
+        return book;
+    }
+
 
     public static void list(String[][] book) {
         for (int i = 0; i < book.length; i++) {
